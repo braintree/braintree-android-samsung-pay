0f67e77192520ad945bae0f29420077c13220abe
sdcoffey <sdcoffey@umich.edu>
2018-04-04 15:46:57 -0700

    Add test cases and test helpers
    Co-authored-by: Tim Martinak <staycalmcodeon@gmail.com>

--- SamsungPay/src/main/java/com/braintreepayments/api/SamsungPay.kt
+++ SamsungPay/src/main/java/com/braintreepayments/api/SamsungPay.kt
@@ -1,81 +1,119 @@
+@file:JvmName("SamsungPay")
+
 package com.braintreepayments.api
 
 import android.os.Bundle
+import android.util.Log
 import com.braintreepayments.api.exceptions.ConfigurationException
 import com.braintreepayments.api.interfaces.BraintreeResponseListener
+import com.braintreepayments.api.internal.ClassHelper
 import com.samsung.android.sdk.samsungpay.v2.PartnerInfo
+import com.samsung.android.sdk.samsungpay.v2.SamsungPay
 import com.samsung.android.sdk.samsungpay.v2.SpaySdk
 import com.samsung.android.sdk.samsungpay.v2.StatusListener
+import com.samsung.android.sdk.samsungpay.v2.payment.CardInfo
+import com.samsung.android.sdk.samsungpay.v2.payment.PaymentInfo
+import com.samsung.android.sdk.samsungpay.v2.payment.PaymentManager
+import java.util.*
 
-object SamsungPay {
+// TODO pull SamsungPay stanza out of out of configuration, look for these values
+private val serviceId = "1f7bb987065d4e16aa5f1f"
+private val merchantId = "sandbox_tmxhyf7d_dcpspy2brwdjr3qn" // TODO pull merchantId out of configuration
+private val merchantName = "bt-dx-integration-test"
 
-    internal val isSamsungPaySDKAvailable: Boolean
-        get() {
-            try {
-                Class.forName("com.samsung.android.sdk.samsungpay.v2.SamsungPay")
-                return true
-            } catch (e: ClassNotFoundException) {
-                return false
+fun isReadyToPay(fragment: BraintreeFragment, listener: BraintreeResponseListener<Boolean>) {
+    if (!isSamsungPayAvailable()) {
+        listener.onResponse(false)
+        fragment.postCallback(ConfigurationException("Samsung SDK not found on classpath. " +
+                "Please add the Samsung Pay SDK as a dependency to your build.gradle file. " +
+                "For more information refer to the docs http://developers.braintreepayments.com/todo/docs/link/here"))
+
+        return
+    }
+
+    getPartnerInfo(fragment, BraintreeResponseListener { info ->
+        val samsungPay = SamsungPay(fragment.applicationContext, info)
+
+        samsungPay.getSamsungPayStatus(object : StatusListener {
+            override fun onSuccess(status: Int, bundle: Bundle) {
+                when (status) {
+                    com.samsung.android.sdk.samsungpay.v2.SamsungPay.SPAY_NOT_SUPPORTED,
+                    com.samsung.android.sdk.samsungpay.v2.SamsungPay.SPAY_NOT_READY -> listener.onResponse(false)
+                    com.samsung.android.sdk.samsungpay.v2.SamsungPay.SPAY_READY -> listener.onResponse(true)
+                }
             }
 
-        }
+            override fun onFail(errorCode: Int, bundle: Bundle) {
+                fragment.postCallback(ConfigurationException("SamsungPay is not available"))
+            }
+        })
+    })
+}
 
-    fun isReadyToPay(fragment: BraintreeFragment, listener: BraintreeResponseListener<Boolean>) {
-        if (!isSamsungPaySDKAvailable) {
-            listener.onResponse(false)
-            fragment.postCallback(ConfigurationException("Samsung SDK not found on classpath." +
-                    "Please add the Samsung Pay SDK as a depdency to your build.gradle file." +
-                    "For more information refer to the docs http://developers.braintreepayments.com/todo/docs/link/here"))
-        }
+// TODO: find appropriate testing values and flesh this out when configuration is complete
+fun startSamsungPay(fragment: BraintreeFragment, paymentInfoBuilder: PaymentInfo.Builder) {
+    getPartnerInfo(fragment, BraintreeResponseListener { info ->
+        // TODO remove and pull these values from SamsungPayConfiguration
+        var brandsFromConfiguration: ArrayList<String> = ArrayList()
+        brandsFromConfiguration.add("Visa")
+        brandsFromConfiguration.add("Mastercard")
 
-        fragment.waitForConfiguration { configuration ->
-            val hardCodedServiceId = "1f7bb987065d4e16aa5f1f"
-            // TODO pull SamsungPay stanza out of out of configuration, look for serviceId
+        paymentInfoBuilder.setMerchantName(merchantName)
+                .setMerchantId(merchantId)
+                .setAllowedCardBrands(samsungPayAcceptedCardBrands(brandsFromConfiguration))
 
-            val info = PartnerInfo(hardCodedServiceId)
+        val paymentManager = PaymentManager(fragment.applicationContext, info)
 
-            val samsungPay = com.samsung.android.sdk.samsungpay.v2.SamsungPay(fragment.applicationContext, info)
+        paymentManager.startInAppPay(paymentInfoBuilder.build(), TransactionInfoListener())
+    })
+}
 
-            samsungPay.getSamsungPayStatus(object : StatusListener {
-                override fun onSuccess(status: Int, bundle: Bundle) {
-                    when (status) {
-                        com.samsung.android.sdk.samsungpay.v2.SamsungPay.SPAY_NOT_SUPPORTED,
-                        com.samsung.android.sdk.samsungpay.v2.SamsungPay.SPAY_NOT_READY -> listener.onResponse(false)
-                        com.samsung.android.sdk.samsungpay.v2.SamsungPay.SPAY_READY -> listener.onResponse(true)
-                    }
-                }
+private fun samsungPayAcceptedCardBrands(braintreeAcceptedCardBrands: ArrayList<String>): List<SpaySdk.Brand> {
+    var samsungAcceptedList = ArrayList<SpaySdk.Brand>()
 
-                override fun onFail(errorCode: Int, bundle: Bundle) {
-                    fragment.postCallback(ConfigurationException("SamsungPay is not available"))
-                }
-            })
+    for (braintreeAcceptedCardBrand in braintreeAcceptedCardBrands) {
+        when (braintreeAcceptedCardBrand.toLowerCase()) {
+            "visa" -> samsungAcceptedList.add(SpaySdk.Brand.VISA)
+            "mastercard" -> samsungAcceptedList.add(SpaySdk.Brand.MASTERCARD)
+            "discover" -> samsungAcceptedList.add(SpaySdk.Brand.DISCOVER)
+            "american express" -> samsungAcceptedList.add(SpaySdk.Brand.AMERICANEXPRESS)
         }
     }
 
-    // TODO: find appropriate testing values and flesh this out when configuration is complete
-//    fun createPaymentBuilder(fragment: BraintreeFragment, listener: BraintreeResponseListener<PaymentInfo.Builder>) {
-//        fragment.waitForConfiguration { configuration ->
-//            var builder = PaymentInfo.Builder()
-//                    .setMerchantName(configuration.samsungPayConfiguration.merchantName)
-//                    .setMerchantId(configuration.samsungPayConfiguration.merchantId)
-//                    .setAllowedCardBrands(samsungPayAcceptedCardBrands(configuration.samsungPayConfiguration.acceptedCardBrands))
-//
-//            listener.onResponse(builder)
-//        }
-//    }
+    return samsungAcceptedList
+}
 
-    private fun samsungPayAcceptedCardBrands(braintreeAcceptedCardBrands: ArrayList<String>) : List<SpaySdk.Brand> {
-        var samsungAcceptedList = ArrayList<SpaySdk.Brand>()
+private fun getPartnerInfo(fragment: BraintreeFragment, listener: BraintreeResponseListener<PartnerInfo>) {
+    fragment.waitForConfiguration { configuration ->
+        val bundle = Bundle()
+        bundle.putString(SamsungPay.PARTNER_SERVICE_TYPE, SpaySdk.ServiceType.INAPP_PAYMENT.toString())
 
-        for (braintreeAcceptedCardBrand in braintreeAcceptedCardBrands) {
-            when (braintreeAcceptedCardBrand) {
-                "visa" -> samsungAcceptedList.add(SpaySdk.Brand.VISA)
-                "mastercard" -> samsungAcceptedList.add(SpaySdk.Brand.MASTERCARD)
-                "discover" -> samsungAcceptedList.add(SpaySdk.Brand.DISCOVER)
-                "american express" -> samsungAcceptedList.add(SpaySdk.Brand.AMERICANEXPRESS)
-            }
-        }
+        val info = PartnerInfo(serviceId, bundle)
 
-        return samsungAcceptedList
+        listener.onResponse(info)
+    }
+}
+
+private fun isSamsungPayAvailable(): Boolean {
+    return ClassHelper.isClassAvailable("com.samsung.android.sdk.samsungpay.v2.SamsungPay")
+}
+
+class TransactionInfoListener: PaymentManager.TransactionInfoListener {
+    override fun onSuccess(response: PaymentInfo?, paymentCredential: String?, extraPaymentData: Bundle?) {
+        Log.d("TransactionInfoListener", "Success")
+        Log.d("TransactionInfoListener", paymentCredential)
+    }
+
+    override fun onFailure(errorCode: Int, errorData: Bundle?) {
+        Log.d("TransactionInfoListener", "failure")
+        Log.d("TransactionInfoListener", errorCode.toString())
+    }
+
+    override fun onAddressUpdated(paymentInfo: PaymentInfo?) {
+        Log.d("TransactionInfoListener", "addressUpdated")
+    }
+
+    override fun onCardInfoUpdated(cardInfo: CardInfo?) {
+        Log.d("TransactionInfoListener", "cardUpdated")
     }
 }
--- SamsungPay/src/test/java/com/braintreepayments/api/SamsungPayUnitTest.java
+++ SamsungPay/src/test/java/com/braintreepayments/api/SamsungPayUnitTest.java
@@ -1,23 +1,125 @@
 package com.braintreepayments.api;
 
+import com.braintreepayments.api.exceptions.ConfigurationException;
+import com.braintreepayments.api.interfaces.BraintreeErrorListener;
+import com.braintreepayments.api.interfaces.BraintreeResponseListener;
+import com.braintreepayments.api.internal.ClassHelper;
+
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.rule.PowerMockRule;
 import org.robolectric.RobolectricTestRunner;
 
+import java.util.concurrent.CountDownLatch;
+
+import static com.braintreepayments.api.test.FixturesHelper.stringFromFixture;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.fail;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
 
 @RunWith(RobolectricTestRunner.class)
-@PowerMockIgnore({ "org.mockito.*", "org.robolectric.*", "android.*", "org.json.*", "com.visa.*" })
+@PowerMockIgnore({ "org.mockito.*", "org.robolectric.*", "android.*", "org.json.*", "com.samsung.*" })
+@PrepareForTest({ ClassHelper.class })
 public class SamsungPayUnitTest {
 
+	@Rule
+	public PowerMockRule mPowerMockRule = new PowerMockRule();
+
+	private BraintreeFragment mBraintreeFragment;
+
+	@Before
+	public void setup() {
+		mBraintreeFragment = new MockFragmentBuilder()
+				.configuration(stringFromFixture("configuration/with_samsung_pay.json"))
+				.build();
+
+		mBraintreeFragment.addListener(new BraintreeErrorListener() {
+			@Override
+			public void onError(Exception error) {
+				throw new RuntimeException(error);
+			}
+		});
+	}
+
+	@Test(timeout = 1000)
+	@SuppressWarnings("unchecked")
+	public void isReadyToPay_whenSDKNotAvailable_returnsFalse() throws ClassNotFoundException {
+		mockStatic(ClassHelper.class);
+		when(ClassHelper.isClassAvailable(eq("com.samsung.android.sdk.samsungpay.v2.SamsungPay"))).thenReturn(false);
+
+		final CountDownLatch latch = new CountDownLatch(1);
+
+		SamsungPay.isReadyToPay(mBraintreeFragment, new BraintreeResponseListener<Boolean>() {
+			@Override
+			public void onResponse(Boolean aBoolean) {
+				assertFalse(aBoolean);
+
+				latch.countDown();
+			}
+		});
+	}
+
+	@Test//(timeout = 1000)
+	public void isReadyToPay_whenSDKNotAvailable_postsException() throws InterruptedException {
+		mockStatic(ClassHelper.class);
+		when(ClassHelper.isClassAvailable(eq("com.samsung.android.sdk.samsungpay.v2.SamsungPay"))).thenReturn(false);
+
+		BraintreeFragment braintreeFragment = new MockFragmentBuilder()
+				.build();
+		final CountDownLatch latch = new CountDownLatch(1);
+
+		SamsungPay.isReadyToPay(braintreeFragment, new BraintreeResponseListener<Boolean>() {
+			@Override
+			public void onResponse(Boolean aBoolean) {
+				assertFalse(aBoolean);
+				latch.countDown();
+			}
+		});
+
+		ArgumentCaptor<ConfigurationException> argumentCaptor = ArgumentCaptor.forClass(ConfigurationException.class);
+		verify(braintreeFragment, times(1)).postCallback(argumentCaptor.capture());
+		assertEquals("TODO final value", argumentCaptor.getValue().getMessage());
+
+		latch.await();
+	}
+
 	@Test
-	public void isReadyToPay_whenSDKNotAvailable_returnsFalse() {
+	public void isReadyToPay_whenSpayStatusIsNotSupported_returnsFalse() {
 		fail("not implemented");
 	}
 
 	@Test
-	public void isReadyToPay_whenSDKNotAvailable_postsException() {
+	public void isReadToPay_whenSpayStatusIsNotReady_returnsFalse() {
+		fail("not implemented");
+	}
+
+	@Test
+	public void isReadyToPay_whenSpayStatusIsReady_returnsTrue() {
+		fail("not implemented");
+	}
+
+	@Test
+	public void createPaymentBuilder_returnsPaymentBuilderWithMerchantName() {
+		fail("not implemented");
+	}
+
+	@Test
+	public void createPaymentBuilder_returnsPaymentBuilderWithMerchantId() {
+		fail("not implemented");
+	}
+
+	@Test
+	public void createPaymentBuilder_returnsPaymentBuilderWithAcceptedCardBrands() {
 		fail("not implemented");
 	}
 }
