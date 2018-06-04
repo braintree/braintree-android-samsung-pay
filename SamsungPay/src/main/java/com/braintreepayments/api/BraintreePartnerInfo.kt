e3e37f7a5914aa68d23aa7089b9c899268c39163
quinnjn <quinn@neumiiller.com>
2018-06-04 11:47:41 -0700

    WIP hardcode service ID

--- SamsungPay/src/main/java/com/braintreepayments/api/BraintreePartnerInfo.kt
+++ SamsungPay/src/main/java/com/braintreepayments/api/BraintreePartnerInfo.kt
@@ -1,9 +1,14 @@
 package com.braintreepayments.api
 
 import android.os.Bundle
-import com.braintreepayments.api.models.SamsungPayConfiguration;
+import com.braintreepayments.api.models.SamsungPayConfiguration
 import com.samsung.android.sdk.samsungpay.v2.PartnerInfo
-import com.samsung.android.sdk.samsungpay.v2.SpaySdk
 
-internal class BraintreePartnerInfo(data: Bundle,
-                                    val configuration: SamsungPayConfiguration): PartnerInfo(configuration.serviceId, data)
+internal class BraintreePartnerInfo(
+    data: Bundle,
+    val configuration: SamsungPayConfiguration
+) : PartnerInfo(
+    configuration.serviceId.let {
+        // TODO("Temporarily set the Service ID")
+        "bb8dcda46bd1400db8fecd"
+    }, data)
