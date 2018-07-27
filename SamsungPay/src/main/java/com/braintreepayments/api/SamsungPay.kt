e9c71748d029a3793b26eebb39e0250c10f67c97
quinnjn <quinn@neumiiller.com>
2018-07-27 13:33:26 -0700

    Improve Kotlin support for merchants
    - `@file:JvmName` only improves Java access.
    - Adding the companion object is how Kotlin determines those
      methods are static.
    - Implying @JvmStatic on the functions lets the JVM know these
      are static methods.

--- SamsungPay/src/main/java/com/braintreepayments/api/SamsungPay.kt
+++ SamsungPay/src/main/java/com/braintreepayments/api/SamsungPay.kt
@@ -1,5 +1,3 @@
-@file:JvmName("SamsungPay")
-
 package com.braintreepayments.api
 
 import android.os.Bundle
@@ -20,7 +18,6 @@ import com.samsung.android.sdk.samsungpay.v2.payment.PaymentManager
 import org.json.JSONObject
 import java.util.*
 
-const val SPAY_NO_SUPPORTED_CARDS_IN_WALLET = -10000
 
 class SamsungPayAvailability() {
     var status: Int = SPAY_NOT_SUPPORTED
@@ -38,213 +35,245 @@ class SamsungPayAvailability() {
     }
 }
 
-fun goToUpdatePage(fragment: BraintreeFragment) {
-    getPartnerInfo(fragment, BraintreeResponseListener { braintreePartnerInfo ->
-        val samsungPay = getSamsungPay(fragment, braintreePartnerInfo)
+class SamsungPay {
+    companion object {
+        const val SPAY_NO_SUPPORTED_CARDS_IN_WALLET = -10000
 
-        samsungPay.goToUpdatePage()
-    })
-}
+        @JvmStatic
+        fun goToUpdatePage(fragment: BraintreeFragment) {
+            getPartnerInfo(fragment, BraintreeResponseListener { braintreePartnerInfo ->
+                val samsungPay = getSamsungPay(fragment, braintreePartnerInfo)
 
-fun activateSamsungPay(fragment: BraintreeFragment) {
-    getPartnerInfo(fragment, BraintreeResponseListener { braintreePartnerInfo ->
-        val samsungPay = getSamsungPay(fragment, braintreePartnerInfo)
+                samsungPay.goToUpdatePage()
+            })
+        }
 
-        samsungPay.activateSamsungPay()
-    })
-}
+        @JvmStatic
+        fun activateSamsungPay(fragment: BraintreeFragment) {
+            getPartnerInfo(fragment, BraintreeResponseListener { braintreePartnerInfo ->
+                val samsungPay = getSamsungPay(fragment, braintreePartnerInfo)
 
-/**
- * Call isReadyToPay before starting your Samsung Pay flow. isReadyToPay will call you back with the
- * status of Samsung Pay. If the Samsung Pay jar has not been included, or if the status of
- * Samsung Pay is anything but [SamsungPayStatus.SPAY_READY], the listener will be called back
- * with a value of false. If the Samsung Pay callback fails and returns an error, that error
- * will be posted to the [BraintreeErrorListener] callback attached to the instance of
- * [BraintreeFragment] passed in here.
- *
- * TODO(Modify this s.t. the response listener also passes the reason for readiness, so that the merchant can take available actions)
- *
- * @param [fragment] TODO
- * @param [listener] TODO
- */
-fun isReadyToPay(fragment: BraintreeFragment, listener: BraintreeResponseListener<SamsungPayAvailability>) {
-    if (!isSamsungPayAvailable()) {
-        listener.onResponse(SamsungPayAvailability(SPAY_NOT_SUPPORTED, Bundle()))
-        return
-    }
+                samsungPay.activateSamsungPay()
+            })
+        }
 
-    getPartnerInfo(fragment, BraintreeResponseListener { braintreePartnerInfo ->
-        val samsungPay = getSamsungPay(fragment, braintreePartnerInfo)
+        /**
+         * Call isReadyToPay before starting your Samsung Pay flow. isReadyToPay will call you back with the
+         * status of Samsung Pay. If the Samsung Pay jar has not been included, or if the status of
+         * Samsung Pay is anything but [SamsungPayStatus.SPAY_READY], the listener will be called back
+         * with a value of false. If the Samsung Pay callback fails and returns an error, that error
+         * will be posted to the [BraintreeErrorListener] callback attached to the instance of
+         * [BraintreeFragment] passed in here.
+         *
+         * TODO(Modify this s.t. the response listener also passes the reason for readiness, so that the merchant can take available actions)
+         *
+         * @param [fragment] TODO
+         * @param [listener] TODO
+         */
+        @JvmStatic
+        fun isReadyToPay(fragment: BraintreeFragment, listener: BraintreeResponseListener<SamsungPayAvailability>) {
+            if (!isSamsungPayAvailable()) {
+                listener.onResponse(SamsungPayAvailability(SPAY_NOT_SUPPORTED, Bundle()))
+                return
+            }
 
-        samsungPay.getSamsungPayStatus(object : StatusListener {
-            override fun onSuccess(status: Int, bundle: Bundle) {
-                val availability = SamsungPayAvailability(status, bundle)
-                if (status != SPAY_READY) {
-                    listener.onResponse(availability)
-                    return
+            getPartnerInfo(fragment, BraintreeResponseListener { braintreePartnerInfo ->
+                val samsungPay = getSamsungPay(fragment, braintreePartnerInfo)
+
+                samsungPay.getSamsungPayStatus(object : StatusListener {
+                    override fun onSuccess(status: Int, bundle: Bundle) {
+                        val availability = SamsungPayAvailability(status, bundle)
+                        if (status != SPAY_READY) {
+                            listener.onResponse(availability)
+                            return
+                        }
+
+                        requestCardInfo(
+                            fragment,
+                            braintreePartnerInfo,
+                            BraintreeResponseListener<SamsungPayAvailability?> { cardInfoAvailability ->
+                                listener.onResponse(cardInfoAvailability ?: availability)
+                            })
+                    }
+
+                    override fun onFail(errorCode: Int, bundle: Bundle) {
+                        fragment.postCallback(SamsungPayException(errorCode, bundle))
+                    }
+                })
+            })
+        }
+
+        // Returns SamsungPayAvailability iff there are no supported cards in the Samsung Pay app, otherwise returns null.
+        @JvmStatic
+        private fun requestCardInfo(
+            fragment: BraintreeFragment,
+            braintreePartnerInfo: BraintreePartnerInfo,
+            listener: BraintreeResponseListener<SamsungPayAvailability?>
+        ) {
+            val paymentManager = getPaymentManager(fragment, braintreePartnerInfo)
+            paymentManager.requestCardInfo(Bundle(), object : PaymentManager.CardInfoListener {
+                override fun onResult(cardResponse: MutableList<CardInfo>?) {
+                    if (cardResponse == null) {
+                        listener.onResponse(SamsungPayAvailability(SPAY_NOT_READY, SPAY_NO_SUPPORTED_CARDS_IN_WALLET))
+                        return
+                    }
+
+                    val acceptedCardBrands =
+                        getAcceptedCardBrands(braintreePartnerInfo.configuration.supportedCardBrands)
+                    val customerCardBrands = cardResponse.map { response -> response.brand }
+                    if (customerCardBrands.intersect(acceptedCardBrands).isEmpty()) {
+                        listener.onResponse(SamsungPayAvailability(SPAY_NOT_READY, SPAY_NO_SUPPORTED_CARDS_IN_WALLET))
+                        return
+                    }
+
+                    listener.onResponse(null)
                 }
 
-                requestCardInfo(fragment, braintreePartnerInfo, BraintreeResponseListener<SamsungPayAvailability?> { cardInfoAvailability ->
-                        listener.onResponse(cardInfoAvailability ?: availability)
-                })
-            }
-
-            override fun onFail(errorCode: Int, bundle: Bundle) {
-                fragment.postCallback(SamsungPayException(errorCode, bundle))
-            }
-        })
-    })
-}
-
-// Returns SamsungPayAvailability iff there are no supported cards in the Samsung Pay app, otherwise returns null.
-private fun requestCardInfo(fragment: BraintreeFragment,
-                    braintreePartnerInfo: BraintreePartnerInfo,
-                    listener: BraintreeResponseListener<SamsungPayAvailability?>)
-{
-    val paymentManager = getPaymentManager(fragment, braintreePartnerInfo)
-    paymentManager.requestCardInfo(Bundle(), object : PaymentManager.CardInfoListener {
-        override fun onResult(cardResponse: MutableList<CardInfo>?) {
-            if (cardResponse == null) {
-                listener.onResponse(SamsungPayAvailability(SPAY_NOT_READY, SPAY_NO_SUPPORTED_CARDS_IN_WALLET))
-                return
-            }
-
-            val acceptedCardBrands = getAcceptedCardBrands(braintreePartnerInfo.configuration.supportedCardBrands)
-            val customerCardBrands = cardResponse.map { response -> response.brand }
-            if (customerCardBrands.intersect(acceptedCardBrands).isEmpty()) {
-                listener.onResponse(SamsungPayAvailability(SPAY_NOT_READY, SPAY_NO_SUPPORTED_CARDS_IN_WALLET))
-                return
-            }
-
-            listener.onResponse(null)
+                override fun onFailure(errorCode: Int, bundle: Bundle?) {
+                    fragment.postCallback(SamsungPayException(errorCode, bundle))
+                }
+            })
         }
 
-        override fun onFailure(errorCode: Int, bundle: Bundle?) {
-            fragment.postCallback(SamsungPayException(errorCode, bundle))
+        /**
+         * Creates a {@link CustomSheetPaymentInfo.Builder} with the merchant ID, merchant name, and allowed
+         * card brands populated by the Braintree merchant account configuration.
+         * @param fragment - {@link BraintreeFragment}
+         * @param listener {@link BraintreeResponseListener<CustomSheetPaymentInfo.Builder>} - listens for
+         * the Braintree flavored {@link CustomSheetPaymentInfo.Builder}.
+         */
+        @JvmStatic
+        fun createPaymentInfo(
+            fragment: BraintreeFragment,
+            listener: BraintreeResponseListener<CustomSheetPaymentInfo.Builder>
+        ) {
+            getPartnerInfo(fragment, BraintreeResponseListener { braintreePartnerInfo ->
+                val paymentInfo = CustomSheetPaymentInfo.Builder()
+                    .setMerchantName(braintreePartnerInfo.configuration.merchantDisplayName)
+                    .setMerchantId(braintreePartnerInfo.configuration.samsungAuthorization)
+                    .setAllowedCardBrands(getAcceptedCardBrands(braintreePartnerInfo.configuration.supportedCardBrands))
+                listener.onResponse(paymentInfo)
+            })
         }
-    })
-}
 
-/**
- * Creates a {@link CustomSheetPaymentInfo.Builder} with the merchant ID, merchant name, and allowed
- * card brands populated by the Braintree merchant account configuration.
- * @param fragment - {@link BraintreeFragment}
- * @param listener {@link BraintreeResponseListener<CustomSheetPaymentInfo.Builder>} - listens for
- * the Braintree flavored {@link CustomSheetPaymentInfo.Builder}.
- */
-fun createPaymentInfo(
-    fragment: BraintreeFragment,
-    listener: BraintreeResponseListener<CustomSheetPaymentInfo.Builder>
-) {
-    getPartnerInfo(fragment, BraintreeResponseListener { braintreePartnerInfo ->
-        var paymentInfo = CustomSheetPaymentInfo.Builder()
-                .setMerchantName(braintreePartnerInfo.configuration.merchantDisplayName)
-                .setMerchantId(braintreePartnerInfo.configuration.samsungAuthorization)
-                .setAllowedCardBrands(getAcceptedCardBrands(braintreePartnerInfo.configuration.supportedCardBrands))
-        listener.onResponse(paymentInfo)
-    })
-}
+        /**
+         * Creates a [PaymentManager] that can communicate with Braintree.
+         * @param [fragment] [BraintreeFragment]
+         * @param [listener] Returns the [PaymentManager]
+         */
+        @JvmStatic
+        fun createPaymentManager(
+            fragment: BraintreeFragment,
+            listener: BraintreeResponseListener<PaymentManager>
+        ) {
+            getPartnerInfo(fragment, BraintreeResponseListener { braintreePartnerInfo ->
+                val paymentManager = getPaymentManager(fragment, braintreePartnerInfo)
+                listener.onResponse(paymentManager)
+            })
+        }
 
-/**
- * Creates a [PaymentManager] that can communicate with Braintree.
- * @param [fragment] [BraintreeFragment]
- * @param [listener] Returns the [PaymentManager]
- */
-fun createPaymentManager(
-    fragment: BraintreeFragment,
-    listener: BraintreeResponseListener<PaymentManager>
-) {
-    getPartnerInfo(fragment, BraintreeResponseListener { braintreePartnerInfo ->
-        val paymentManager = getPaymentManager(fragment, braintreePartnerInfo)
-        listener.onResponse(paymentManager)
-    })
-}
+        /**
+         * [requestPayment] takes a CustomSheetInfo.Builder and starts intitiates the Samsung Pay flow
+         * with some custom UI provided by you.
+         *
+         * @param [fragment] TODO
+         * @param [customSheetPaymentInfo] TODO
+         * @param [listener] TODO
+         */
+        @JvmStatic
+        fun requestPayment(
+            fragment: BraintreeFragment,
+            customSheetPaymentInfo: CustomSheetPaymentInfo,
+            listener: SamsungPayCustomTransactionUpdateListener
+        ) {
+            createPaymentManager(fragment, BraintreeResponseListener {
+                requestPayment(fragment, it, customSheetPaymentInfo, listener)
+            })
+        }
 
-/**
- * [requestPayment] takes a CustomSheetInfo.Builder and starts intitiates the Samsung Pay flow
- * with some custom UI provided by you.
- *
- * @param [fragment] TODO
- * @param [customSheetPaymentInfo] TODO
- * @param [listener] TODO
- */
-fun requestPayment(
-    fragment: BraintreeFragment,
-    customSheetPaymentInfo: CustomSheetPaymentInfo,
-    listener: SamsungPayCustomTransactionUpdateListener
-) {
-    createPaymentManager(fragment, BraintreeResponseListener {
-        requestPayment(fragment, it, customSheetPaymentInfo, listener)
-    })
-}
+        /**
+         * [requestPayment] takes a CustomSheetInfo.Builder and starts intitiates the Samsung Pay flow
+         * with some custom UI provided by you.
+         *
+         * @param [fragment] TODO
+         * @param [paymentManager] TODO
+         * @param [customSheetPaymentInfo] TODO
+         * @param [listener] TODO
+         */
+        @JvmStatic
+        fun requestPayment(
+            fragment: BraintreeFragment,
+            paymentManager: PaymentManager,
+            customSheetPaymentInfo: CustomSheetPaymentInfo,
+            listener: SamsungPayCustomTransactionUpdateListener
+        ) {
+            paymentManager.startInAppPayWithCustomSheet(
+                customSheetPaymentInfo,
+                SamsungPayCustomTransactionListenerWrapper(fragment, paymentManager, listener)
+            )
+        }
 
-/**
- * [requestPayment] takes a CustomSheetInfo.Builder and starts intitiates the Samsung Pay flow
- * with some custom UI provided by you.
- *
- * @param [fragment] TODO
- * @param [paymentManager] TODO
- * @param [customSheetPaymentInfo] TODO
- * @param [listener] TODO
- */
-fun requestPayment(
-    fragment: BraintreeFragment,
-    paymentManager: PaymentManager,
-    customSheetPaymentInfo: CustomSheetPaymentInfo,
-    listener: SamsungPayCustomTransactionUpdateListener
-) {
-    paymentManager.startInAppPayWithCustomSheet(
-        customSheetPaymentInfo,
-        SamsungPayCustomTransactionListenerWrapper(fragment, paymentManager, listener)
-    )
-}
+        /**
+         * @return true if the SamsungPay SDK is available in the classpath, i.e. you have included
+         * the Samsung Pay jar file in your declared app dependencies.
+         */
+        @JvmStatic
+        fun isSamsungPayAvailable(): Boolean {
+            return ClassHelper.isClassAvailable("com.samsung.android.sdk.samsungpay.v2.SamsungPay")
+        }
 
-/**
- * @return true if the SamsungPay SDK is available in the classpath, i.e. you have included
- * the Samsung Pay jar file in your declared app dependencies.
- */
-fun isSamsungPayAvailable(): Boolean {
-    return ClassHelper.isClassAvailable("com.samsung.android.sdk.samsungpay.v2.SamsungPay")
-}
+        @JvmStatic
+        private fun getAcceptedCardBrands(configurationBrands: Set<String>): List<SpaySdk.Brand> {
+            val samsungAcceptedList = ArrayList<SpaySdk.Brand>()
 
-private fun getAcceptedCardBrands(configurationBrands: Set<String>): List<SpaySdk.Brand> {
-    val samsungAcceptedList = ArrayList<SpaySdk.Brand>()
+            for (braintreeAcceptedCardBrand in configurationBrands) {
+                when (braintreeAcceptedCardBrand.toLowerCase()) {
+                    "visa" -> samsungAcceptedList.add(SpaySdk.Brand.VISA)
+                    "mastercard" -> samsungAcceptedList.add(SpaySdk.Brand.MASTERCARD)
+                    "discover" -> samsungAcceptedList.add(SpaySdk.Brand.DISCOVER)
+                    "american_express" -> samsungAcceptedList.add(SpaySdk.Brand.AMERICANEXPRESS)
+                }
+            }
 
-    for (braintreeAcceptedCardBrand in configurationBrands) {
-        when (braintreeAcceptedCardBrand.toLowerCase()) {
-            "visa" -> samsungAcceptedList.add(SpaySdk.Brand.VISA)
-            "mastercard" -> samsungAcceptedList.add(SpaySdk.Brand.MASTERCARD)
-            "discover" -> samsungAcceptedList.add(SpaySdk.Brand.DISCOVER)
-            "american_express" -> samsungAcceptedList.add(SpaySdk.Brand.AMERICANEXPRESS)
+            return samsungAcceptedList
+        }
+
+        @JvmStatic
+        private fun getPartnerInfo(
+            fragment: BraintreeFragment,
+            listener: BraintreeResponseListener<BraintreePartnerInfo>
+        ) {
+            fragment.waitForConfiguration { configuration ->
+                val bundle = Bundle()
+
+                bundle.putString(PARTNER_SERVICE_TYPE, ServiceType.INAPP_PAYMENT.toString())
+                bundle.putBoolean(
+                    PaymentManager.EXTRA_KEY_TEST_MODE,
+                    configuration.samsungPay.environment.toUpperCase() == "SANDBOX"
+                )
+
+                val clientSdkMetadataJson = JSONObject()
+                clientSdkMetadataJson.put(
+                    "clientSdkMetadata", MetadataBuilder()
+                        .integration(fragment.integrationType)
+                        .sessionId(fragment.sessionId)
+                        .version()
+                        .build()
+                )
+                bundle.putString("additionalData", clientSdkMetadataJson.toString())
+
+                listener.onResponse(BraintreePartnerInfo(configuration.samsungPay, bundle))
+            }
+        }
+
+        @JvmStatic
+        private fun getSamsungPay(fragment: BraintreeFragment, info: PartnerInfo): SamsungPay {
+            return SamsungPay(fragment.activity, info)
+        }
+
+        @JvmStatic
+        private fun getPaymentManager(fragment: BraintreeFragment, info: PartnerInfo): PaymentManager {
+            return PaymentManager(fragment.activity, info)
         }
     }
-
-    return samsungAcceptedList
-}
-
-internal fun getPartnerInfo(fragment: BraintreeFragment, listener: BraintreeResponseListener<BraintreePartnerInfo>) {
-    fragment.waitForConfiguration { configuration ->
-        val bundle = Bundle()
-
-        bundle.putString(PARTNER_SERVICE_TYPE, ServiceType.INAPP_PAYMENT.toString())
-        bundle.putBoolean(PaymentManager.EXTRA_KEY_TEST_MODE, configuration.samsungPay.environment.toUpperCase() == "SANDBOX")
-
-        val clientSdkMetadataJson = JSONObject()
-        clientSdkMetadataJson.put("clientSdkMetadata", MetadataBuilder()
-                .integration(fragment.integrationType)
-                .sessionId(fragment.sessionId)
-                .version()
-                .build())
-        bundle.putString("additionalData", clientSdkMetadataJson.toString())
-
-        listener.onResponse(BraintreePartnerInfo(configuration.samsungPay, bundle))
-    }
-}
-
-internal fun getSamsungPay(fragment: BraintreeFragment, info: PartnerInfo): SamsungPay {
-    return SamsungPay(fragment.activity, info)
-}
-
-internal fun getPaymentManager(fragment: BraintreeFragment, info: PartnerInfo): PaymentManager {
-    return PaymentManager(fragment.activity, info)
 }
