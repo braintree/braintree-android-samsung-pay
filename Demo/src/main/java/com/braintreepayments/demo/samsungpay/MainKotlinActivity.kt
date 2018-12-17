7e223340650e297823e14ed8a53f1fc13d4717f0
Quinn Neumiiller <quinn@getbraintree.com>
2018-12-17 22:26:41 +0000

    Convert to appcompat

--- Demo/src/main/java/com/braintreepayments/demo/samsungpay/MainKotlinActivity.kt
+++ Demo/src/main/java/com/braintreepayments/demo/samsungpay/MainKotlinActivity.kt
@@ -1,6 +1,6 @@
 package com.braintreepayments.demo.samsungpay
 
-import android.app.AlertDialog
+import android.support.v7.app.AlertDialog
 import android.os.Bundle
 import android.support.annotation.IdRes
 import android.support.v7.app.AppCompatActivity
